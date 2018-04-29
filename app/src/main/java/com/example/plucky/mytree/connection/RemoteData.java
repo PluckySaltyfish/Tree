14809c7e1f67278e3fea16f63fa44d53e8b0eb16
plucky <1828151761@qq.com>
2018-04-30 02:07:13 +0800

    medal icon

--- app/src/main/java/com/example/plucky/mytree/connection/RemoteData.java
+++ app/src/main/java/com/example/plucky/mytree/connection/RemoteData.java
@@ -140,6 +140,15 @@ public class RemoteData {
         list.add(1);
         list.add(2);
         list.add(3);
+        list.add(18);
+        list.add(5);
+        list.add(7);
+        list.add(11);
+        list.add(12);
+        list.add(13);
+        list.add(14);
+        list.add(8);
+        list.add(6);
         return list;
     }
 
--- app/src/main/java/com/example/plucky/mytree/fragment/achievement/Achievement.java
+++ app/src/main/java/com/example/plucky/mytree/fragment/achievement/Achievement.java
@@ -1,6 +1,7 @@
 package com.example.plucky.mytree.fragment.achievement;
 
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 
 import com.example.plucky.mytree.R;
 import com.example.plucky.mytree.connection.RemoteData;
@@ -11,8 +12,10 @@ public class Achievement {
     private int id[]=new int[30];
     private int image[]=new int[30];
     private String names[]={};
-    private int status[]=new int[30];
+    //status用来存储用户是否获取此勋章
+    private int status[]=new int[18];
     private RemoteData mRemoteData;
+    private Context mContext;
     //0--未获得
     //1--已获得
 
@@ -32,21 +35,33 @@ public class Achievement {
     }
 
     private void loadImage(){
-        for (int i = 0 ;i < 30 ;i++){
+        ApplicationInfo appInfo = mContext.getApplicationInfo();
+
+        for (int i = 0 ;i < 18 ;i++){
+            int num = i+1;
+            String name_prefix;
             if (status[i]==0){
                 //加载灰色图片
+                name_prefix="n_medal";
             }
             else{
                 //加载彩色图片
+                name_prefix="medal";
             }
+            int res= mContext.getResources().getIdentifier(name_prefix+num, "drawable", appInfo.packageName);
+            image[i]=res;
         }
     }
 
     public Achievement(Context mContext,String username){
+        this.mContext = mContext;
+        for (int i = 0;i<18;i++){
+            status[i]=0;
+        }
         mRemoteData = new RemoteData(mContext);
         List<Integer>list = mRemoteData.getAchievementID(username);
         for (int i = 0;i<list.size();i++){
-            status[list.get(i)]=1;
+            status[list.get(i)-1]=1;
         }
         loadImage();
     }
